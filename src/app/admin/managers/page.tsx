e6718b6dcfe1cf8c6fbbc5c0ec8bc910aff1a702
thangndse173512 <thangndse173512@fpt.edu.vn>
2025-01-10 20:08:37 +0700

    admin page UI

---
+++ src/app/admin/managers/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminManageManagersPage() {
+    return (
+        <div>Managers</div>
+    )
+}
---
+++ src/app/admin/supervisors/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminManageSupervisorsPage() {
+    return (
+        <div>Supervisors</div>
+    )
+}
