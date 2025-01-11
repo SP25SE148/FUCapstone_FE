8c5779434185ee5126996ac1464b71d94d0c65be
thangndse173512 <thangndse173512@fpt.edu.vn>
2025-01-11 21:51:36 +0700

    Sidebar Component

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,20 @@
-import { AdminSidebar } from "./components/admin-sidebar"
+import { LayoutDashboard, UserRoundCog, UserPen, Users, Newspaper } from "lucide-react"
+
+import { AppSidebar } from "@/components/layout/app-sidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+
+const items = [
+    { title: "Dashboard", url: "/admin", icon: LayoutDashboard, },
+    { title: "Managers", url: "/admin/managers", icon: UserRoundCog, },
+    { title: "Supervisors", url: "/admin/supervisors", icon: UserPen, },
+    { title: "Students", url: "/admin/students", icon: Users, },
+    { title: "Announcements", url: "/admin/announcements", icon: Newspaper, },
+]
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
-            <AdminSidebar />
+            <AppSidebar items={items} />
             <main className="p-4">
                 <SidebarTrigger />
                 {children}
