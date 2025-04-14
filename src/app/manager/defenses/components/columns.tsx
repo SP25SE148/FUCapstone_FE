748163476d32b1433dcf572a2962cd3fd8be213a
thangndse173512 <thangndse173512@fpt.edu.vn>
2025-04-14 16:43:00 +0700

    fix deploy 3

--- src/app/manager/defenses/components/columns.tsx
+++ src/app/manager/defenses/components/columns.tsx
@@ -1,33 +1,34 @@
 "use client"
 
 import type { ColumnDef } from "@tanstack/react-table"
-import { DataTableColumnHeader } from "@/components/ui/data-table-column-header"
-import type { DefenseCalendarItem } from "@/contexts/supervisor/supervisor-defense-context"
-import { getDateNoTime } from "@/lib/utils"
-import { Building2, Calendar, Clock, Clock1, Group, Hash, Locate, LocateFixedIcon, LocateIcon, LocateOff, PlayCircle, TicketMinus, Users2 } from 'lucide-react'
+import { Building2, Calendar, Clock, Hash, TicketMinus, Users2 } from 'lucide-react'
 
+import { getDateNoTime } from "@/lib/utils"
+import { DefenseCalendarItem } from "@/types/types"
+
+import { DataTableColumnHeader } from "@/components/ui/data-table-column-header"
 
 export const columns: ColumnDef<DefenseCalendarItem>[] = [
   {
     accessorKey: "defenseDate",
-    header: ({ column }) => <DataTableColumnHeader column={column} title="Defense Date" icon={<Calendar className="h-4 w-4"/>} />,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Defense Date" icon={<Calendar className="h-4 w-4" />} />,
     cell: ({ row }) => <span>{getDateNoTime(row.original.defenseDate)}</span>,
   },
   {
     accessorKey: "topicCode",
-    header: ({ column }) => <DataTableColumnHeader column={column} title="Topic Code" icon={<Hash className="h-4 w-4"/>}/>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Topic Code" icon={<Hash className="h-4 w-4" />} />,
     cell: ({ row }) => <span>{row.original.topicCode}</span>,
   },
   {
     accessorKey: "groupCode",
-    header: ({ column }) => <DataTableColumnHeader column={column} title="Group Code" icon ={<Users2 className="h-4 w-4" />}/>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Group Code" icon={<Users2 className="h-4 w-4" />} />,
     cell: ({ row }) => <span>{row.original.groupCode}</span>,
   },
   {
     accessorKey: "defendAttempt",
     header: ({ column }) => (
       <div className="flex justify-center w-full">
-        <DataTableColumnHeader column={column} title="Defend Attempt" icon = {<TicketMinus className="h-4 w-4" />} />
+        <DataTableColumnHeader column={column} title="Defend Attempt" icon={<TicketMinus className="h-4 w-4" />} />
       </div>
     ),
     cell: ({ row }) => <div className="text-center">{row.original.defendAttempt}</div>,
@@ -36,14 +37,14 @@ export const columns: ColumnDef<DefenseCalendarItem>[] = [
     accessorKey: "slot",
     header: ({ column }) => (
       <div className="flex justify-center w-full">
-        <DataTableColumnHeader column={column} title="Slot" icon= {<Clock className="h-4 w-4" /> }/>
+        <DataTableColumnHeader column={column} title="Slot" icon={<Clock className="h-4 w-4" />} />
       </div>
     ),
     cell: ({ row }) => <div className="text-center">{row.original.slot}</div>,
   },
   {
     accessorKey: "location",
-    header: ({ column }) => <DataTableColumnHeader column={column} title="Location" icon = {<Building2 className="h-4 w-4" />}/>,
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Location" icon={<Building2 className="h-4 w-4" />} />,
     cell: ({ row }) => <span>{row.original.location}</span>,
   }
 ]
